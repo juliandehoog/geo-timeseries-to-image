da4a76fc83f5a22161188903d00e2e37e5e51dc0
Julian de Hoog <julian@dehoog.ca>
2021-08-05 14:35:20 +1000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name="geots2img",
-      version="0.1.1",
+      version="0.1.2",
       description="Geo Time Series to Image",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url="https://github.com/juliandehoog/geo-timeseries-to-image",
       author="Julian de Hoog",
       author_email='julian@dehoog.ca',
